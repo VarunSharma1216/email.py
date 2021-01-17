85bf3faba23c3c7d8cb2debf8bffe5b88aa07c93
darthkolli145 <77557182+darthkolli145@users.noreply.github.com>
2021-01-17 11:16:28 -0800

    Update emails.py

--- emails.py
+++ emails.py
@@ -1,5 +1,6 @@
 import smtplib
 from tkinter import *
+import getpass
 root = Tk()
 root.title("email")
 root.geometry("400x400")
@@ -7,14 +8,13 @@ root.mainloop()
 smtp_object = smtplib.SMTP('smtp.gmail.com',587)
 smtp_object.ehlo()
 smtp_object.starttls()
-import getpass
-def login():
+def login(self):
     email = getpass.getpass('Email: ')
     password = getpass.getpass('Password: ')
     smtp_object.login(email,password)
 from_address = email
 to_address = input("Enter the emails of whoever you want to send it to: ").split()
-def send_email():
+def send_email(self):
     subject = input('Enter the subject line: ')
     name = input("Who are you sending the email to: ")
     body = input("What is the message you want to send: " )
