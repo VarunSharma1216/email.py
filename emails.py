eb0362af4960d31f2083852f5f16adb94ab15da8
VarunSharma1216 <74435849+VarunSharma1216@users.noreply.github.com>
2021-01-17 13:14:26 -0800

    Update emails.py

--- emails.py
+++ emails.py
@@ -9,33 +9,53 @@ class MyWindow:
         self.lbl1=Label(win, text='Email:')
         self.lbl2=Label(win, text='Password:')
         self.lbl3=Label(win, text='Email address of person you want to send the email to:')
+        self.lbl4=Label(win, text='Subject:')
+        self.lbl5=Label(win, text='Main Message:')
         self.t1=Entry(bd=3)
         self.t2=Entry()
         self.t3=Entry()
-        self.b2 = Button(win, text='Send email',command = self.sendEmail)
+        self.t4=Entry()
+        self.t5=Entry()
+        self.b2 = Button(win, text='Send email:',command = self.sendEmail)
+        self.b3 = Button(win, text='Formal',command = self.formal)
+        self.b4 = Button(win, text='Informal',command = self.informal)
         self.lbl1.place(x=100, y=50)
-        self.t1.place(x=200, y=50)
         self.lbl2.place(x=100, y=100)
-        self.t2.place(x=200, y=100)
         self.b1=Button(win, text='Login', command=self.login)
         self.b1.place(x=100, y=150)
+        self.t1.place(x=200, y=50)
+        self.t2.place(x=200, y=100)
+    
     def login(self):
         global from_address 
         from_address = str(self.t1.get())
         password = str(self.t2.get())
         smtp_object.login(from_address,password)
+        
         self.lbl3.place(x=100, y=200)
+        self.t3.place(x=450, y=200)
         self.b2.place(x = 100, y = 250)
-        self.t3.place(x=400, y=200)
+        self.b3.place(x=100, y=300)
+        self.b4.place(x = 200, y = 300)
+        self.lbl4.place(x = 100, y = 350)
+        self.t4.place(x = 200, y = 350)
+        self.t5.place(x = 200, y = 400)
+        
+    def formal(self):
+        global m
+        global body
+        m = "Dear, " + name + ","+ "\n" +body +"\n"+"Regards,"+"\n"+your_name
+    
+    def informal(self):
+        m = "Hello, " + name + ","+ "\n" +body +"\n"+"Thanks,"+"\n"+your_name
     def sendEmail(self):
-        subject = 'Hello'
-        message =  greeting + m + endgreeting
-        to_address = self.t3.get()
-        msg = 'Subject: '+subject+'\n'+message
+        subject = self.t4.get()
+        m = slef.t5.get()
+        msg = 'Subject: '+subject+'\n'+ m
         smtp_object.sendmail(from_address,to_address,msg)
         
 window=Tk()
 mywin=MyWindow(window)
-window.title('email')
-window.geometry("500x400+10+10")
+window.title('Hello Python')
+window.geometry("400x300+10+10")
 window.mainloop()
