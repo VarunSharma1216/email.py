014963c0773545138b3a8c9f3d7d583d6f074a7e
VarunSharma1216 <74435849+VarunSharma1216@users.noreply.github.com>
2021-01-17 12:07:25 -0800

    Update emails.py

--- emails.py
+++ emails.py
@@ -5,38 +5,37 @@ smtp_object = smtplib.SMTP('smtp.gmail.com',587)
 smtp_object.ehlo()
 smtp_object.starttls()
 class MyWindow:
-        def __init__(self, win):
-                self.lbl1=Label(win, text='email')
-                self.lbl2=Label(win, text='Second number')
-                self.t1=Entry(bd=3)
-                self.t2=Entry()
-                self.btn1 = Button(win, text='login')
-                self.btn2=Button(win, text='Subtract')
-                self.lbl1.place(x=100, y=50)
-                self.t1.place(x=200, y=50)
-                self.lbl2.place(x=100, y=100)
-                self.t2.place(x=200, y=100)
-                self.b1=Button(win, text='login', command=self.login)
-                self.b2=Button(win, text='Subtract')
-                self.b2.bind('<Button-1>', self.sub)
-                self.b1.place(x=100, y=150)
-                self.b2.place(x=200, y=150)
-        def login(self):
-                email = getpass.getpass('Email: ')
-                password = getpass.getpass('Password: ')
-                smtp_object.login(email,password)
-                from_address = email
-                to_address = input("Enter the emails of whoever you want to send it to: ").split()
-        def send_email(self):
-                subject = input('Enter the subject line: ')
-                name = input("Who are you sending the email to: ")
-                body = input("What is the message you want to send: " )
-                your_name = input("Enter your full name: ")
-                message = "Hello " + name+ ","+ "\n" +body +"\n"+"Thanks,"+"\n"+your_name
-                msg = 'Subject: '+subject+'\n'+message
-                smtp_object.sendmail(from_address,to_address,msg)
-send_email()
-root = Tk()
-root.title("email")
-root.geometry("400x400")
-root.mainloop()
+    def __init__(self, win):
+        self.lbl1=Label(win, text='Email:')
+        self.lbl2=Label(win, text='Password:')
+        self.lbl3=Label(win, text='Email address of person you want to send the email to:')
+        self.t1=Entry(bd=3)
+        self.t2=Entry()
+        self.t3=Entry()
+        self.b2 = Button(win, text='Send email:',command = self.sendEmail)
+        self.lbl1.place(x=100, y=50)
+        self.t1.place(x=200, y=50)
+        self.lbl2.place(x=100, y=100)
+        self.t2.place(x=200, y=100)
+        self.lbl3.place(x=100, y=200)
+        self.t3.place(x=450, y=200)
+        self.b1=Button(win, text='Login', command=self.login)
+        self.b1.place(x=100, y=150)
+        self.b2.place(x = 100, y = 250)
+    def login(self):
+        global from_address 
+        from_address = str(self.t1.get())
+        password = str(self.t2.get())
+        smtp_object.login(from_address,password)
+    def sendEmail(self):
+        subject = 'hi'
+        message = 'hi'
+        to_address = 'anshul.kolli@gmail.com'
+        msg = 'Subject: '+subject+'\n'+message
+        smtp_object.sendmail(from_address,to_address,msg)
+        
+window=Tk()
+mywin=MyWindow(window)
+window.title('Hello Python')
+window.geometry("400x300+10+10")
+window.mainloop()
